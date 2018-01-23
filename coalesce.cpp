bcc5b29c7ffbd883e139c14328bd4f56468d5a7e
Vishesh Chanana <visheshchanana1@gmail.com>
2018-01-22 16:10:27 -0800

    Able to take data from arguments

--- coalesce.cpp
+++ coalesce.cpp
@@ -5,7 +5,7 @@
 
 
 
-/* coalesce - this command line utility manipulates 
+/* coalesce - this command line utility manipulates
     and compares point cloud data with CAD data
  *
  * This application ...
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 
 
 #include<iostream>
@@ -22,10 +23,24 @@
 
 using namespace std;
 
+struct globalArgs_t {
+    int noIndex;                                /* -I option */
+    char *langCode;                             /* -l option */
+    const char *outFileName;    /* -o option */
+    FILE *outFile;
+    int verbosity;                              /* -v option */
+    char **inputFiles;                  /* input files */
+    int numInputFiles;                  /* number of input files */
+} globalArgs;
+int numInputFiles;                  /* # of input files */
+
+static const char *optString = "Il:o:i:vh?";
+
+
 int main( int argc, char *argv[] )
 {
     int opt = 0;
-    
+    int arg_count = 1;
     /* Initialize globalArgs before we get to work. */
     globalArgs.noIndex = 0;             /* false */
     globalArgs.langCode = NULL;
@@ -34,56 +49,61 @@ int main( int argc, char *argv[] )
     globalArgs.verbosity = 0;
     globalArgs.inputFiles = NULL;
     globalArgs.numInputFiles = 0;
-    
-    /* Process the arguments with getopt(), then 
-     * populate globalArgs. 
+
+    /* Process the arguments with getopt(), then
+     * populate globalArgs.
      */
+
+     /* Vishesh comment */
     opt = getopt( argc, argv, optString );
     while( opt != -1 ) {
         switch( opt ) {
-            case 'a':
-                globalArgs.noIndex = 1; /* true */
-                break;
-                
-            case 'l':
-                globalArgs.langCode = optarg;
-                break;
-                
-            case 'o':
-                /* This generates an "assignment from
-                 * incompatible pointer type" warning that
-                 * you can safely ignore.
-                 */
-                globalArgs.outFileName = optarg;
-                break;
-                
-            case 'v':
-                globalArgs.verbosity++;
-                break;
-                
-            case 'h':   /* fall-through is intentional */
-            case '?':
-                display_usage();
-                break;
-                
-            default:
-                /* You won't actually get here. */
-                break;
-        }
-        
-        opt = getopt( argc, argv, optString );
-    }
-    
-    globalArgs.inputFiles = argv + optind;
-    globalArgs.numInputFiles = argc - optind;
+              case 'i':
+                  cout<<"goes into this"<<endl;
+                  break;
+            // case 'a':
+            //     globalArgs.noIndex = 1; /* true */
+            //     break;
+            //
+            // case 'l':
+            //     globalArgs.langCode = optarg;
+            //     break;
+            //
+            // case 'o':
+            //     /* This generates an "assignment from
+            //      * incompatible pointer type" warning that
+            //      * you can safely ignore.
+            //      */
+            //     globalArgs.outFileName = optarg;
+            //     break;
+            //
+            // case 'v':
+            //     globalArgs.verbosity++;
+            //     break;
+            //
+            // case 'h':   /* fall-through is intentional */
+            // case '?':
+            //     display_usage();
+            //     break;
+            //
+            // default:
+            //     /* You won't actually get here. */
+            //     break;
+         }
 
-    convert_document();
-    
+         opt = getopt( argc, argv, optString );
+     }
 
+    //globalArgs.inputFiles = argv + optind;
+    //globalArgs.numInputFiles = argc - optind;
+
+    //convert_document();
+    void convert_document();
     string data;
     int count = 0;
     //specify the path here
-    ifstream file("F:\Ekta Flow\SBD Nosecone.asc");
+    //D:\\EKTA\\Coalesce\\files\\SBD Nosecone.asc
+    ifstream file(argv[2]);
     if(!file.is_open()){
       cout<<"Error opening file"<<endl;
       return 0;
@@ -91,7 +111,6 @@ int main( int argc, char *argv[] )
     while(!file.eof()){
       getline(file,data);
       count++;
-      cout<<data<<endl;
     }
      cout<<count<<endl;
      return 0;
@@ -99,27 +118,16 @@ int main( int argc, char *argv[] )
 
 
 /* doc2html supports the following command-line arguments:
- * 
+ *
  * -I - don't produce a keyword index
  * -l lang - produce output in the specified language, lang
  * -o outfile - write output to outfile instead of stdout
  * -v - be verbose; more -v means more diagnostics
  * additional file names are used as input files
- * 
+ *
  * The optString global tells getopt() which options we
  * support, and which options have arguments.
  */
-struct globalArgs_t {
-    int noIndex;                                /* -I option */
-    char *langCode;                             /* -l option */
-    const char *outFileName;    /* -o option */
-    FILE *outFile;
-    int verbosity;                              /* -v option */
-    char **inputFiles;                  /* input files */
-    int numInputFiles;                  /* # of input files */
-} globalArgs;
-
-static const char *optString = "Il:o:vh?";
 
 /* Display program usage, and exit.
  */

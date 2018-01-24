d2b255e960978caa5ff73a18de425281d689666e
Vishesh Chanana <visheshchanana1@gmail.com>
2018-01-24 02:05:08 -0800

    Added -i, -o, -s, -a functionality

--- coalesce.cpp
+++ coalesce.cpp
@@ -5,7 +5,7 @@
 
 
 
-/* coalesce - this command line utility manipulates 
+/* coalesce - this command line utility manipulates
     and compares point cloud data with CAD data
  *
  * This application ...
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 
 
 #include<iostream>
@@ -23,13 +24,13 @@
 using namespace std;
 
 /* doc2html supports the following command-line arguments:
- * 
+ *
  * -I - don't produce a keyword index
  * -l lang - produce output in the specified language, lang
  * -o outfile - write output to outfile instead of stdout
  * -v - be verbose; more -v means more diagnostics
  * additional file names are used as input files
- * 
+ *
  * The optString global tells getopt() which options we
  * support, and which options have arguments.
  */
@@ -39,11 +40,19 @@ struct globalArgs_t {
     const char *outFileName;    /* -o option */
     FILE *outFile;
     int verbosity;                              /* -v option */
-    char **inputFiles;                  /* input files */
+    char *inputFiles;                  /* input files */
     int numInputFiles;                  /* # of input files */
+    bool summary_f;                 /* flag for summary */
+    bool convert_f;                 /* flag for convert */
+    bool analyze_f;                 /* flag for analyze */
+    bool verbose_f;                 /* flag for verbose */
 } globalArgs;
 
-static const char *optString = "Il:o:vh?";
+static const char *optString = "o:i:savh?";
+
+int no_of_points();
+void point_cloud_summary();
+void display_detailed_description();
 
 /* Display program usage, and exit.
  */
@@ -57,7 +66,7 @@ void display_usage( void )
 int main( int argc, char *argv[] )
 {
     int opt = 0;
-    
+
     /* Initialize globalArgs before we get to work. */
     globalArgs.noIndex = 0;             /* false */
     globalArgs.langCode = NULL;
@@ -66,21 +75,29 @@ int main( int argc, char *argv[] )
     globalArgs.verbosity = 0;
     globalArgs.inputFiles = NULL;
     globalArgs.numInputFiles = 0;
-    
-    /* Process the arguments with getopt(), then 
-     * populate globalArgs. 
+    globalArgs.summary_f = false;
+    globalArgs.convert_f = false;
+    globalArgs.analyze_f = false;
+    globalArgs.verbose_f = false;
+
+    /* Process the arguments with getopt(), then
+     * populate globalArgs.
      */
     opt = getopt( argc, argv, optString );
     while( opt != -1 ) {
-        switch( opt ) {
-            case 'a':
-                globalArgs.noIndex = 1; /* true */
+      switch( opt ) {
+            case 'i':
+                globalArgs.inputFiles = optarg;
                 break;
-                
+
+            case 'a':
+                globalArgs.analyze_f = true;
+                break;
+
             case 'l':
                 globalArgs.langCode = optarg;
                 break;
-                
+
             case 'o':
                 /* This generates an "assignment from
                  * incompatible pointer type" warning that
@@ -88,44 +105,88 @@ int main( int argc, char *argv[] )
                  */
                 globalArgs.outFileName = optarg;
                 break;
-                
-            case 'v':
-                globalArgs.verbosity++;
+            case 's':
+                if(globalArgs.outFileName == NULL)
+                      globalArgs.outFileName = "summary.txt";
+                globalArgs.summary_f = true;
                 break;
-                
+            case 'v':
+                //globalArgs.verbosity++;
+                globalArgs.verbose_f = true;
+                break;
+
+            case 'c':
+                globalArgs.convert_f = true;
+                break;
+
+
             case 'h':   /* fall-through is intentional */
             case '?':
                 display_usage();
                 break;
-                
+
             default:
+
                 /* You won't actually get here. */
                 break;
         }
-        
+
         opt = getopt( argc, argv, optString );
     }
-    
 
-    
+    if(globalArgs.summary_f == true || (globalArgs.convert_f == false && globalArgs.analyze_f == false && globalArgs.verbose_f == false)){
+      point_cloud_summary();
+    }
 
-    string data;
-    int count = 0;
-    //specify the path here
-    ifstream file("F:\\Ekta Flow\\SBD Nosecone.asc");
-    if(!file.is_open()){
-      cout<<"Error opening file"<<endl;
-      return 0;
+    if(globalArgs.analyze_f == true){
+      display_detailed_description();
     }
-    while(!file.eof()){
-      getline(file,data);
-      count++;
-      cout<<data<<endl;
-    }
-     cout<<count<<endl;
      return 0;
 }
 
+/* Count the number of points in the point cloud */
+int no_of_points(){
+  string data;
+  int count = 0;
+  ifstream file(globalArgs.inputFiles);
+  if(!file.is_open()){
+    cout<<"Error opening file"<<endl;
+    return 0;
+  }
+  while(!file.eof()){
+    getline(file,data);
+    count++;
+  }
+  file.close();
+  return count;
+}
 
+/* Create the summary of the point cloud */
+void point_cloud_summary(){
+  ofstream outfile (globalArgs.outFileName);
+  if(outfile.is_open()){
+    outfile <<"=========================SUMMARY=========================" << endl << endl;
+    outfile << "Total number of points in the point cloud: "<<no_of_points() << endl;
+    outfile.close();
+  }
+  else{
+    cout << "Error. Unable to open file"<<endl;
+  }
+}
 
-
+/* Print point cloud analysis(temporary) */
+void display_detailed_description(){
+  string data;
+  int count = 0;
+  ifstream file(globalArgs.inputFiles);
+  if(!file.is_open()){
+    cout<<"Error opening file"<<endl;
+  }
+  while(!file.eof()){
+    getline(file,data);
+    cout << data <<endl;
+    count++;
+  }
+  file.close();
+  cout << endl << "Total number of points: "<< count << endl;
+}
